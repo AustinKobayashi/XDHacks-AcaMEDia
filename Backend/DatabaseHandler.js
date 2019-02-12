f49ded67310a6289b35906dd6d4bb3a86fc6e1b5
Austin Kobayashi <austinkobayashi@gmail.com>
2019-02-12 15:41:50 -0800

    added connection pooling

--- Backend/DatabaseHandler.js
+++ Backend/DatabaseHandler.js
@@ -1,29 +1,56 @@
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
-    //host: "104.198.97.44",
-    socketPath: "/cloudsql/t-solstice-224300:us-west1:acamedia-db",
-    user: "root",
-    password: "yogi03",
-    database  : 'acamedia'
-});
+// var con = mysql.createConnection({
+//     //host: "104.198.97.44",
+//     socketPath: "/cloudsql/t-solstice-224300:us-west1:acamedia-db",
+//     user: "root",
+//     password: "yogi03",
+//     database  : 'acamedia'
+// });
+//
+// con.connect(function(err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+// });
 
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
+var pool = mysql.createPool({
+    connectionLimit : 100, //important
+    //host     : '127.0.0.1',
+    
+    user     : 'root',
+    password : 'yogi03',
+    database : 'acamedia',
+    debug    :  false
 });
-
 
 class DatabaseHandler {
 
     static query_db (query, callback) {
-        con.query(query, function (err, result) {
-            if (err){
-                console.warn(query);
+        pool.getConnection(function(err,connection){
+            if (err) {
                 throw err;
             }
-            return callback(result);
+
+            console.log('connected as id ' + connection.threadId);
+
+            connection.query(query ,function(err, rows){
+                connection.release();
+                if(!err) {
+                    callback(rows);
+                }
+            });
+
+            connection.on('error', function(err) {
+                throw err;
+            });
         });
+        // con.query(query, function (err, result) {
+        //     if (err){
+        //         console.warn(query);
+        //         throw err;
+        //     }
+        //     return callback(result);
+        // });
     }
 }
 
