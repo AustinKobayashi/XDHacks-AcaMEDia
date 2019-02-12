c2d53ec04c3583274d194e437e6a00a68e6fd6db
Giulio Rossi <ciuffi9@mac.com>
2019-02-11 20:38:07 -0800

    Add cloud functions for database querying

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const QueryEngine = require('./QueryEngine.js');
+const DatabaseBuilder = require('./CloudDatabaseBuilder.js');
 
 /* GET users listing. */
 exports.Location = (req, res) => {
@@ -19,5 +20,34 @@ exports.Query = (req, res) => {
     QueryEngine.query(req.query.term, req.query.location).then((results) => {
         console.log(results);
         res.send(results);
+    });
+};
+
+exports.QueryPubmed = (req, res) => {
+    let query_term = req.body.query_term;
+    let query_term_parts = query_term.split(" ");
+    let query = query_term_parts.join("+");
+
+    let lab_id = parseInt(req.body.lab_id);
+
+    DatabaseBuilder.query_pubmed(query, lab_id, (result) => {
+        if (result){
+            res.send(200)
+        }else{
+            res.send(500)
+        }
+    });
+};
+exports.InsertPubmedArticles = (req, res) => {
+
+    let article_id = parseInt(req.query.id);
+    let lab_id = parseInt(req.query.lab_id);
+
+    DatabaseBuilder.get_and_insert_articles(article_id, lab_id,(result) => {
+        if (result){
+            res.send(200)
+        }else{
+            res.send(500)
+        }
     });
 };
